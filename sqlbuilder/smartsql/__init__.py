d19f4e7309c7ceb9ab6b44c286d95b0732de0885
ivan <none@none>
2012-03-17 07:17:46 +0200

    Backed out changeset 7be42ff3f01a

--- sqlbuilder/smartsql/__init__.py
+++ sqlbuilder/smartsql/__init__.py
@@ -165,7 +165,6 @@ class Expr(object):
             if len(other) < 1:
                 raise Error("Empty list is not allowed")
             sql = ", ".join([PLACEHOLDER for i in xrange(len(other))])
-            sql = "({0})".format(sql)
             other = Expr(sql, other)
         return Condition("IN", self, other)
 
@@ -174,7 +173,6 @@ class Expr(object):
             if len(other) < 1:
                 raise Error("Empty list is not allowed")
             sql = ", ".join([PLACEHOLDER for i in xrange(len(other))])
-            sql = "({0})".format(sql)
             other = Expr(sql, other)
         return Condition("NOT IN", self, other)
 
