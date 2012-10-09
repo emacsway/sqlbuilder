410666f166fea2a6902ba57c60b1d4dd1bc2a1e3
ivan <none@none>
2012-10-09 15:28:52 +0300

    metadata.py: hg + git supporting

--- metadata.py
+++ metadata.py
@@ -8,19 +8,28 @@ try:
         p = subprocess.Popen(['hg', 'parents', r'--template={rev}\n'],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if not p.returncode:
-            fh = open('HGREV', 'w')
+            fh = open('VCSREV', 'w')
             fh.write(p.communicate()[0].splitlines()[0])
             fh.close()
+
+    elif os.path.isdir('.git'):
+        p = subprocess.Popen(['git', 'rev-parse', r'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if not p.returncode:
+            fh = open('VCSREV', 'w')
+            fh.write(p.communicate()[0].splitlines()[0])
+            fh.close()
+
 except (OSError, IndexError):
     pass
     
 try:
-    hgrev = open('HGREV').read()
+    rev = open('VCSREV').read()
 except IOError:
-    hgrev = ''
+    rev = ''
 
 name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 authors = 'Ivan Zakrevsky'
 copyright_years = '2011-2012'
-version = '0.7.post%s' % hgrev
+version = '0.7.post{0}'.format(rev)
 release = version
