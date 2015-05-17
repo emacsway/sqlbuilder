57f3630ec3385b2dd3e80cc152484d974996871a
Ivan <ivzak@yandex.ru>
2015-05-17 19:35:55 +0300

    0.7.9.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ app_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
     name = app_name,
-    version = '0.7.9.2',
+    version = '0.7.9.3',
 
     packages = find_packages(),
     include_package_data=True,
