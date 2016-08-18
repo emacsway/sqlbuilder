2a7a4822e22f4a333e81d847fc9cbbf9bf0e8d56
Ivan <ivzak@yandex.ru>
2016-08-18 17:48:24 +0300

    backward compatibility with sqlbuilder.django_sqlbuilder.compilers

---
+++ sqlbuilder/django_sqlbuilder/compilers/__init__.py
@@ -0,0 +1,4 @@
+from sqlbuilder.smartsql import warn
+
+warn('sqlbuilder.django_sqlbuilder.compilers', 'sqlbuilder.django_sqlbuilder.dialects')
+__path__.insert(0, 'sqlbuilder/django_sqlbuilder/dialects')
