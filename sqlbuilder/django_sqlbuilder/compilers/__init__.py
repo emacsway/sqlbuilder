860582650118ef37d7ad5404f7505b07ae838515
Ivan <ivzak@yandex.ru>
2016-08-27 02:48:58 +0300

    Use os.path.realpath() in sqlbuilder.smartsql.compilers and sqlbuilder.django_sqlbuilder.compilers

--- sqlbuilder/django_sqlbuilder/compilers/__init__.py
+++ sqlbuilder/django_sqlbuilder/compilers/__init__.py
@@ -3,5 +3,8 @@ import sys
 from sqlbuilder.smartsql import warn
 
 warn('sqlbuilder.django_sqlbuilder.compilers', 'sqlbuilder.django_sqlbuilder.dialects')
-__path__.insert(0, os.path.join(os.path.dirname(os.path.dirname(sys.modules[__name__].__file__)), 'dialects'))
+__path__.insert(0, os.path.join(
+    os.path.dirname(os.path.dirname(os.path.realpath(sys.modules[__name__].__file__))),
+    'dialects'
+))
 
