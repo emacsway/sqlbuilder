14d0c59253631c9db1784e49ad155cbfcc2c34f7
Ivan <ivzak@yandex.ru>
2017-02-02 17:35:15 +0200

    Inheritable OperatorRegistry
    --HG--
    branch : operable

--- sqlbuilder/smartsql/__init__.py
+++ sqlbuilder/smartsql/__init__.py
@@ -292,18 +292,37 @@ class OperatorRegistry(object):
     class OperatorNotFound(Error):
         pass
 
-    def __init__(self):
-        self._data = {}
+    def __init__(self, parent=None):
+        self._children = weakref.WeakKeyDictionary()
+        self._parents = []
+        self._local_registry = {}
+        self._registry = {}
+        if parent:
+            self._parents.extend(parent._parents)
+            self._parents.append(parent)
+            parent._children[self] = True
+            self._update_cache()
+
+    def create_child(self):
+        return self.__class__(self)
 
     def register(self, operator, operands, result_type, expression_factory):
-        self._data[(operator, operands)] = (result_type, expression_factory)
+        self._registry[(operator, operands)] = (result_type, expression_factory)
+        self._update_cache()
 
     def get(self, operator, operands):
         try:
-            return self._data[(operator, operands)]
+            return self._registry[(operator, operands)]
         except KeyError:
             # raise self.OperatorNotFound(operator, operands)
             return (BaseType, lambda l, r: Binary(l, operator, r))
+
+    def _update_cache(self):
+        for parent in self._parents:
+            self._registry.update(parent._local_registry)
+        self._registry.update(self._local_registry)
+        for child in self._children:
+            child._update_cache()
 
 operator_registry = OperatorRegistry()
 
