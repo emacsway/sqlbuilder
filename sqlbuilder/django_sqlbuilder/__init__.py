336ed19586966e19d344c0a06cab968280a2a41c
Ivan <ivzak@yandex.ru>
2017-02-04 05:10:19 +0200

    minor fix for backward compatibility sqlbuilder.django_sqlbuilder

--- sqlbuilder/django_sqlbuilder/__init__.py
+++ sqlbuilder/django_sqlbuilder/__init__.py
@@ -5,5 +5,5 @@ import warnings
 warnings.warn("sqlbuilder.django_sqlbuilder is deprecated. Use django_sqlbuilder instead", PendingDeprecationWarning, stacklevel=2)
 __path__.insert(0, os.path.join(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(sys.modules[__name__].__file__)))),
-    'django_cache_dependencies'
+    'django_sqlbuilder'
 ))
