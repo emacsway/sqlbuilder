b741f760271f611d0e5798d417d2aab073b5c1ec
Ivan <ivzak@yandex.ru>
2017-02-04 05:06:46 +0200

    sqlbuilder/django_sqlbuilder -> django_sqlbuilder

---
+++ sqlbuilder/django_sqlbuilder/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import warnings
+
+warnings.warn("sqlbuilder.django_sqlbuilder is deprecated. Use django_sqlbuilder instead", PendingDeprecationWarning, stacklevel=2)
+__path__.insert(0, os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(sys.modules[__name__].__file__)))),
+    'django_cache_dependencies'
+))
