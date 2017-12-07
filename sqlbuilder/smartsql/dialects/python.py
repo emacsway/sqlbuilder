73d06e2825ea00b82a98e2a4c7110ac5336a24ba
Ivan <ivzak@yandex.ru>
2017-12-07 03:49:17 +0200

    begin to work on python executor of query

---
+++ sqlbuilder/smartsql/dialects/python.py
@@ -0,0 +1,58 @@
+import copy
+import weakref
+from sqlbuilder.smartsql.constants import CONTEXT
+
+
+class Executor(object):
+
+    def __init__(self, parent=None):
+        self._children = weakref.WeakKeyDictionary()
+        self._parents = []
+        self._local_registry = {}
+        self._registry = {}
+        if parent:
+            self._parents.extend(parent._parents)
+            self._parents.append(parent)
+            parent._children[self] = True
+            self._update_cache()
+
+    def create_child(self):
+        return self.__class__(self)
+
+    def when(self, cls):
+        def deco(func):
+            self._local_registry[cls] = func
+            self._update_cache()
+            return func
+        return deco
+
+    def _update_cache(self):
+        for parent in self._parents:
+            self._registry.update(parent._local_registry)
+        self._registry.update(self._local_registry)
+        for child in self._children:
+            child._update_cache()
+
+
+    def __call__(self, expr, state=None):
+        pass
+
+
+class State(object):
+
+    def __init__(self):
+        self._stack = []
+        self.auto_tables = []
+        self.join_tables = []
+        self.context = CONTEXT.QUERY
+
+    def push(self, attr, new_value=None):
+        old_value = getattr(self, attr, None)
+        self._stack.append((attr, old_value))
+        if new_value is None:
+            new_value = copy.copy(old_value)
+        setattr(self, attr, new_value)
+        return old_value
+
+    def pop(self):
+        setattr(self, *self._stack.pop(-1))
